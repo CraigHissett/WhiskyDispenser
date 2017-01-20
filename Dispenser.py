2fbf0b2bf1c33b6510255ff7a70cbdddf2bc0c50
Craig Hissett <CraigHissett@users.noreply.github.com>
2017-01-20 11:10:02 +0000

    Update and rename main.py to Dispenser.py

--- Dispenser.py
+++ Dispenser.py
@@ -8,7 +8,9 @@ import json
 import time
 import datetime
 import os, sys
-from lcd import *
+import fcntl
+import struct
+import socket
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
@@ -19,7 +21,7 @@ Socket2=1
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
-        lcd_string("Index Requested",LCD_LINE_1)
+
     
     def post (self):
         WebCommand = self.get_argument ('command', '')
@@ -30,13 +32,11 @@ class IndexHandler(tornado.web.RequestHandler):
                 if sys.platform == 'win32':
                     os.system('shutdown /s')
                 else:
-                    lcd_string("Shutting down...",LCD_LINE_2)
                     os.system('shutdown -h now')
             elif WebValue == 'Reboot':
                 if sys.platform == 'win32':
                     os.system('shutdown /r')
                 else:
-                    lcd_string("Rebooting...",LCD_LINE_2)
                     os.system('shutdown -r now')
             else:
                 print('No matching Pi Command')
@@ -51,11 +51,15 @@ class IndexHandler(tornado.web.RequestHandler):
         else:
             print('Command not recognised')
 
-            
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('settings.html')
+
+        
 class GlenHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('Glenmorangie.html')
-        lcd_string("Drinks Dispenser",LCD_LINE_1)
     def post (self):
         WebCommand = self.get_argument ('command', '')
         WebValue = self.get_argument ('value', '')
@@ -101,105 +105,72 @@ class GlenHandler(tornado.web.RequestHandler):
                 print('Unknown drink selection')
         else:
             print('Command not recognised')
-
-class TestHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('index.html')
-
-    def post (self):
-        WebCommand = self.get_argument ('command', '')
-        WebValue = self.get_argument ('value', '')
-        
-        if WebCommand == 'Pi':
-            if WebValue == 'Shutdown':
-                if sys.platform == 'win32':
-                    os.system('shutdown /s')
-                else:
-                    os.system('shutdown -h now')
-            elif WebValue == 'Reboot':
-                if sys.platform == 'win32':
-                    os.system('shutdown /r')
-                else:
-                    os.system('shutdown -r now')
-            else:
-                print('No matching Pi Command')
-                return
-        elif WebCommand == 'Arduino':
-            if WebValue == 'Reset':
-                print('No matching Pi Command')
-                return                
-            else:
-                print('No matching Arduino Command')
-                return
-        else:
-            print('Command not recognised')
-
-
-class IsaacHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('index.html')
-
-    def post (self):
-        WebCommand = self.get_argument ('command', '')
-        WebValue = self.get_argument ('value', '')
-        
-        if WebCommand == 'Pi':
-            if WebValue == 'Shutdown':
-                if sys.platform == 'win32':
-                    os.system('shutdown /s')
-                else:
-                    os.system('shutdown -h now')
-            elif WebValue == 'Reboot':
-                if sys.platform == 'win32':
-                    os.system('shutdown /r')
-                else:
-                    os.system('shutdown -r now')
-            else:
-                print('No matching Pi Command')
-                return
-        elif WebCommand == 'Arduino':
-            if WebValue == 'Reset':
-                print('No matching Pi Command')
-                return                
-            else:
-                print('No matching Arduino Command')
-                return
-        else:
-            print('Command not recognised')
             
-class SocketHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print("WebSocket opened")
+class JackHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('Jack.html')
+    def post (self):
+        WebCommand = self.get_argument ('command', '')
+        WebValue = self.get_argument ('value', '')
+        
+        if WebCommand == 'Pi':
+            if WebValue == 'Shutdown':
+                if sys.platform == 'win32':
+                    os.system('shutdown /s')
+                else:
+                    os.system('shutdown -h now')
+            elif WebValue == 'Reboot':
+                if sys.platform == 'win32':
+                    os.system('shutdown /r')
+                else:
+                    os.system('shutdown -r now')
+            else:
+                print('No matching Pi Command')
+                return
+        elif WebCommand == 'Arduino':
+            if WebValue == 'Reset':
+                print('No matching Pi Command')
+                return                
+            else:
+                print('No matching Arduino Command')
+                return
+        elif WebCommand == 'Pour':
+            if WebValue == '1':
+                print('Drink 1 requested')
+                return
+            elif WebValue == '2':
+                print('Drink 2 requested')
+                return
+            elif WebValue == '3':
+                print('Drink 3 requested')
+                return
+            else:
+                print('Unknown drink selection')
+        else:
+            print('Command not recognised')            
 
-    def on_message(self, requested):
-        response = "Socket" + requested
-        #self.write_message(u"You said: " + )
-        print(response)
-        self.write_message(response)
-
-    def on_close(self):
-        print("WebSocket closed")
-
-
-def UpdateIPs():
-    lcd_string("LAN: " + get_ip_address('eth0'),LCD_LINE_3)
-    lcd_string("WLAN: " + get_ip_address('wlan0'),LCD_LINE_4)    
-
+            
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15])
+        )[20:24])
+    except IOError:
+        return "N/A"            
+            
 
 if __name__ == "__main__":
-    lcd_init()
-    lcd_string("Server Running...",LCD_LINE_1)
-    lcd_string("   ",LCD_LINE_2)
-    lcd_string("LAN: " + get_ip_address('eth0'),LCD_LINE_3)
-    lcd_string("WLAN: " + get_ip_address('wlan0'),LCD_LINE_4)
+
     tornado.options.parse_command_line()
     app = tornado.web.Application(
         handlers=[
             (r"/", IndexHandler),
-            (r"/isaac", IsaacHandler),
-            (r"/socket", SocketHandler),
+            (r"/settings", SettingsHandler),
             (r"/glenmorangie", GlenHandler),
-            (r"/test", TestHandler)],
+            (r"/Jack", JackHandler)],
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             template_path=os.path.join(os.path.dirname(__file__), "templates"))
 
@@ -209,6 +180,6 @@ if __name__ == "__main__":
     print ("Listening on port:", options.port)
     main_loop = tornado.ioloop.IOLoop.instance()
     # Schedule event (5 seconds from now)
-    main_loop.call_later(5, UpdateIPs)
+    main_loop.call_later(5,get_ip_address('wlan0'))
     # Start main loop
     main_loop.start()
